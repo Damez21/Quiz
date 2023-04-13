5e0feb63624cccd65815077290dae0bb2742e420
Damiem Mclaurin <damiemmclaurin@Damiems-MacBook-Pro.local>
2023-04-13 00:37:09 -0400

    made questions in js to later go in my html, trying to figure out my css styling, working on adding more into my html code.

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
 console.log("NBA Quiz")
 
+// Who Was The 1st Overall Pick In The 2003 Nba Draft???
+// Chris Bosh
+// Dwayne Wade
+// Chris Paul
+// Lebron James
 
+// How Many NBA Championships Does Kobe Bryant Have?
+// 2
+// 5
+// 4
+// 6
 
+// In What Season Did The NBA "Bubble" Take Place?
+// 2018-2019
+// 2021-2022
+// 2019-2020
+// 2020-2021
+
+// Who Is The ALL TIME Leader In 3pt Made?
+// Ray Allen
+// Steph Curry
+// Reggie Miller
+// James Harden
+
+// How Many 3 pointers Did Shaquile O'Neal Make In His Career?
+// 1
+// 0
+// 5
+// 25
+
+// Who Was The Youngest Player To Ever Play In An NBA Game?
+// Michael Jordan
+// Kobe Bryant
+// Wilt Chamberlin
+// Andrew Bynum
+
+// Which Player Holds The Most All Time Stats On The U.S. Olympic Team?
+// Bill Russel
+// Carmelo Anthony
+// Lebron James
+// Michael Jordan
+
+// How Many Championships Does Michael Jordan Have?
+// 6
+// 8
+// 5
+// 3
